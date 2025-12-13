080d89cb7009e43f6206de2d1a2ad86acc3ea1f8
和泉 朔真（Izumi Sakuma） <84662681+YunChenqwq@users.noreply.github.com>
2025-12-13 17:55:16 +0800

    qwq
    PWA

--- index.tsx
+++ index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+
+registerSW({
+  immediate: true,
+  onOfflineReady() {
+    console.log('Sagiri Camera 已准备好离线使用');
+  },
+});
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
